d0a809947b751c967593b46ea3662574de3b60e9
Ian Pike <Ianpike98@gmail.com>
2022-07-12 19:03:50 -0400

    Update Program.cs

--- TopKFrequentElements347Medium/Program.cs
+++ TopKFrequentElements347Medium/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace TopKFrequentElements347Medium;
 
@@ -31,12 +32,10 @@ public class Solution {
         return output;
     }
     
-    // Bucket sort implementation.
+    // TODO: Return to this at a later date to properly implement this
+    /*// Extremely slow implementation. Better approach is absolutely out there.
     public static int[] TopKFrequent2(ReadOnlySpan<int> nums, int k)
     {
-        if (nums.Length == 1)
-            return nums.ToArray();
-
         var hash = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++)
         {
@@ -49,38 +48,71 @@ public class Solution {
                 hash.Add(nums[i], 1);
             }
         }
-        
-        // TODO: This needs to be properly implemented as the current approach is broken.
-        var freq = new int[nums.Length + 1];
-        foreach (var (key, val) in hash)
+
+        var output = new int[k];
+        for (int i = 0; i < k; i++)
         {
-            freq[val].;
+            var maxValueKey = hash.MaxBy(x => x.Value).Key;
+            output[i] = maxValueKey;
+            hash.Remove(maxValueKey);
+        }
+        
+        return output;
+    }
+    
+    // Bucket sort implementation.
+    public static int[] TopKFrequent3(ReadOnlySpan<int> nums, int k)
+    {
+        var map = new Dictionary<int, int>();
+        var n = nums.Length;
+        
+        for (var i = 0; i < n; i++)
+        {
+            if (map.ContainsKey(nums[i]))
+            {
+                map[nums[i]]++;
+            }
+            else
+            {
+                map.Add(nums[i], 1);
+            }
+        }
+        
+        var output = map.OrderByDescending(kv => kv.Value).Select(kv => kv.Key);
+        for (int i = 0; i < output.Max() i++)
+        {
+            
+        }
+        
+        var frequency = new List<List<int>>(k + 1);
+        foreach (var (key, val) in map)
+        {
+            frequency[val].Add(key);
+        }
+
+        var result = new int[k];
+        var resultSize = 0;
+        for (int i = frequency.Count; i > 1 || result.Length-1 < k; i--)
+        {
+            if (frequency[i].Capacity != 0)
+            {
+                
+            }
         }
 
         var result = new int[k];
         var resultSize = 0;
 
-        for (int i = freq.Length; result.Length-1 > resultSize; i--)
+        for (int i = frequency.Length; result.Length-1 > resultSize; i--)
         {
-            if (freq[i] != 0)
+            if (frequency[i] != 0)
             {
-                result[resultSize] = freq[i];
+                result[resultSize] = frequency[i];
                 resultSize++;
             }
         }
 
         return result;
-    }
-
-
-    public static void Main()
-    {
-        var nums = new[] {1, 1, 1, 2, 2, 3};
-        var nums2 = new[] {1, 2};
-        var nums3 = new[] {-1, -1};
-        var k = 2;
-
-        var output = TopKFrequent2(nums2, k);
-        Console.WriteLine();
-    }
+    }*/
+    
 }
